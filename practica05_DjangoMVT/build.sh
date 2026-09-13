064d0265e96634bbedf66e298a32001338a770e1
daphneguada2d-dot <daphneguada2d@gmail.com>
2026-09-13 12:34:07 -0500

    Corregir rutas simples en build.sh

--- practica05_DjangoMVT/build.sh
+++ practica05_DjangoMVT/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-cd practica05_DjangoMVT
 python manage.py collectstatic --no-input
 python manage.py migrate
